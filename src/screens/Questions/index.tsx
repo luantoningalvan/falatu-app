40346dd55b4549f44d66b0d4fd15a0c5d886a74e
Luan <ltoningalvan@gmail.com>
2020-06-13 17:16:26 -0300

    Página de perguntas

--- src/screens/Questions/index.tsx
+++ src/screens/Questions/index.tsx
@@ -1,13 +1,102 @@
-import React from 'react';
-import { Container } from './styles';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Container,
+  SectionTitle,
+  Answers,
+  Answer,
+  AnswerIcon,
+  AnswerTitle,
+  AnswerTime,
+  TabsSwitcher,
+  Tabs,
+  Tab,
+  TabTitle,
+  TabContent,
+  Question,
+  QuestionTitle,
+  QuestionIcon,
+  QuestionCount,
+} from './styles';
+import { ScrollView, View } from 'react-native';
+import Header from '../../components/Header';
 import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/Feather';
+import { Text } from 'react-native-svg';
 
 const Questions: React.FC = () => {
+  const [selectedTab, selectTab] = useState('own');
+
   return (
     <Container>
       <LinearGradient colors={['#D90368', '#741960']} style={{ flex: 1 }}>
-        <Text>Questions</Text>
+        <Header>
+          <Icon name="plus-circle" size={24} color="#fff" />
+        </Header>
+        <ScrollView style={{ flex: 1 }}>
+          <SectionTitle>Respostas Recentes</SectionTitle>
+          <Answers>
+            <Answer>
+              <AnswerIcon name="message-square" />
+              <AnswerTitle>Quantos anos eu aparento ter?</AnswerTitle>
+              <AnswerTime>32s</AnswerTime>
+            </Answer>
+            <Answer>
+              <AnswerIcon name="image" />
+              <AnswerTitle>Qual bermuda eu compro?</AnswerTitle>
+              <AnswerTime>1min</AnswerTime>
+            </Answer>
+            <Answer>
+              <AnswerIcon name="image" />
+              <AnswerTitle>Qual bermuda eu compro?</AnswerTitle>
+              <AnswerTime>2min</AnswerTime>
+            </Answer>
+          </Answers>
+
+          <SectionTitle>Suas perguntas</SectionTitle>
+          <TabsSwitcher>
+            <Tabs>
+              <Tab
+                current={selectedTab === 'own'}
+                onPress={() => selectTab('own')}>
+                <TabTitle>Próprias</TabTitle>
+              </Tab>
+              <Tab
+                current={selectedTab === 'default'}
+                onPress={() => selectTab('default')}>
+                <TabTitle>Padrões</TabTitle>
+              </Tab>
+            </Tabs>
+
+            <TabContent>
+              {selectedTab === 'own' && (
+                <View>
+                  <Question>
+                    <QuestionIcon name="image" />
+                    <QuestionTitle>Qual foto?</QuestionTitle>
+                    <QuestionCount>34 respostas</QuestionCount>
+                  </Question>
+                  <Question>
+                    <QuestionIcon name="image" />
+                    <QuestionTitle>Qual foto?</QuestionTitle>
+                    <QuestionCount>34 respostas</QuestionCount>
+                  </Question>
+                  <Question>
+                    <QuestionIcon name="image" />
+                    <QuestionTitle>Qual foto?</QuestionTitle>
+                    <QuestionCount>34 respostas</QuestionCount>
+                  </Question>
+                  <Question>
+                    <QuestionIcon name="image" />
+                    <QuestionTitle>Qual foto?</QuestionTitle>
+                    <QuestionCount>34 respostas</QuestionCount>
+                  </Question>
+                </View>
+              )}
+
+              {selectedTab === 'default' && <Text>Padrões</Text>}
+            </TabContent>
+          </TabsSwitcher>
+        </ScrollView>
       </LinearGradient>
     </Container>
   );
--- src/screens/Questions/styles.ts
+++ src/screens/Questions/styles.ts
@@ -1,5 +1,86 @@
 import styled from 'styled-components/native';
-
+import Icon from 'react-native-vector-icons/Feather';
+import { TouchableOpacityProps } from 'react-native';
 export const Container = styled.View`
   flex: 1;
+`;
+
+export const SectionTitle = styled.Text`
+  color: white;
+  font-family: 'Comfortaa-Bold';
+  font-size: 24px;
+  margin: 16px 32px;
+`;
+
+export const Answers = styled.View`
+  padding: 0px 32px;
+`;
+export const Answer = styled.TouchableOpacity`
+  flex-direction: row;
+  padding: 16px;
+  border-radius: 4px;
+  background: white;
+  align-items: center;
+  margin-bottom: 8px;
+`;
+export const AnswerIcon = styled(Icon)`
+  color: #333;
+  font-size: 16px;
+`;
+export const AnswerTitle = styled.Text`
+  color: #333;
+  flex: 1;
+  font-size: 16px;
+  margin: 0px 16px;
+`;
+export const AnswerTime = styled.Text`
+  color: #333;
+  font-size: 16px;
+`;
+
+export const TabsSwitcher = styled.View``;
+
+interface TabProps extends TouchableOpacityProps {
+  current?: boolean;
+}
+
+export const Tabs = styled.View`
+  flex-direction: row;
+  background: rgba(0, 0, 0, 0.2);
+`;
+
+export const Tab = styled.TouchableOpacity<TabProps>`
+  flex: 1;
+  padding: 16px;
+
+  border-bottom-width: 2px;
+  border-bottom-color: ${props => (props.current ? 'pink' : 'transparent')};
+`;
+
+export const TabTitle = styled.Text`
+  text-align: center;
+  font-size: 18px;
+  color: white;
+  font-family: 'Comfortaa-Regular';
+`;
+export const TabContent = styled.View``;
+
+export const Question = styled.TouchableOpacity`
+  flex-direction: row;
+  padding: 16px;
+  border-radius: 4px;
+  align-items: center;
+  margin-bottom: 8px;
+  border-bottom-width: 1px;
+  border-bottom-color: rgba(255, 255, 255, 0.1);
+`;
+
+export const QuestionTitle = styled(AnswerTitle)`
+  color: white;
+`;
+export const QuestionIcon = styled(AnswerIcon)`
+  color: white;
+`;
+export const QuestionCount = styled.Text`
+  color: white;
 `;
