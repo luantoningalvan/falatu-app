454288788a9c9ef9e8c7ca5c5349edc8ebdbe57f
Luan <ltoningalvan@gmail.com>
2020-06-13 00:59:34 -0300

    Estrutura da página de play

--- src/screens/Play/index.tsx
+++ src/screens/Play/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+  Container,
+  ReportButton,
+  ReportButtonText,
+  SkipButton,
+  SkipButtonText,
+  Header,
+  PlayArea,
+} from './styles';
 import { Text } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/Feather';
+import Logo from '../../../assets/static/wdyt-logo.svg';
 
 const Play: React.FC = () => {
   return (
-    <Container>
-      <LinearGradient colors={['#D90368', '#741960']} style={{ flex: 1 }}>
-        <Text>Play</Text>
-      </LinearGradient>
-    </Container>
+    <LinearGradient colors={['#D90368', '#741960']} style={{ flex: 1 }}>
+      <Container>
+        <Header>
+          <Logo width={100} height={44} />
+          <ReportButton>
+            <ReportButtonText>DENUNCIAR</ReportButtonText>
+            <Icon size={20} name="info" color="white" />
+          </ReportButton>
+        </Header>
+
+        <PlayArea>
+          <Text>Play</Text>
+          <SkipButton>
+            <Icon size={28} name="skip-forward" color="white" />
+            <SkipButtonText>Pular</SkipButtonText>
+          </SkipButton>
+        </PlayArea>
+      </Container>
+    </LinearGradient>
   );
 };
 
--- src/screens/Play/styles.ts
+++ src/screens/Play/styles.ts
@@ -3,3 +3,40 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
 `;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 86px;
+  padding: 16px;
+`;
+
+export const PlayArea = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 32px;
+`;
+
+export const ReportButton = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+`;
+export const ReportButtonText = styled.Text`
+  color: white;
+  font-size: 16px;
+  margin-right: 8px;
+  font-family: 'Comfortaa-Regular';
+`;
+
+export const SkipButton = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+`;
+export const SkipButtonText = styled.Text`
+  color: white;
+  font-size: 20px;
+  margin-left: 8px;
+  font-family: 'Comfortaa-Regular';
+`;
