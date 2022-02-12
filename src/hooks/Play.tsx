f7466130161b68ffd78bd0b90b604a677e2c9859
Luan Tonin Galvan <ltoningalvan@gmail.com>
2022-02-12 11:34:02 -0300

    refactor: change typing of Play context and add some example content

--- src/hooks/Play.tsx
+++ src/hooks/Play.tsx
@@ -13,8 +13,25 @@ interface ReportData {
   reportReason: string;
 }
 
+type Media = {
+  url: string;
+};
+
+export type Option = {
+  text: string;
+  number: number;
+  thumb?: string;
+};
+
+export type Question = {
+  id: string;
+  mode: 'text' | 'binary' | 'comparison' | 'multi' | 'video';
+  title: string;
+  media: Media[];
+  options?: Option[];
+};
 interface PlayContextData {
-  questions: QuestionResponse[];
+  questions: Question[];
   loading: boolean;
   getRandomQuestions(): Promise<void>;
   skipQuestion(): Promise<void>;
@@ -22,32 +39,101 @@ interface PlayContextData {
   reportQuestion(data: ReportData): Promise<void>;
 }
 
-export enum QuestionTypes {
-  YESORNOT = 'yesornot',
-  MULTI = 'multi',
-  PHOTO_COMPARISON = 'photocomp',
-  WRITTEN = 'written',
-}
-
-export interface QuestionResponse {
-  _id: string;
-  randomUserAvatar?: {
-    _id: string;
-    url: string;
-  };
-  title: string;
-  type: QuestionTypes;
-  options: {
-    title?: string;
-    url?: string;
-    answerCount: number;
-  }[];
-}
-
 const PlayContext = createContext<PlayContextData>({} as PlayContextData);
 
 export const PlayProvider: React.FC = ({children}) => {
-  const [queue, setQueue] = useState<QuestionResponse[]>([]);
+  const [queue, setQueue] = useState<Question[]>([
+    {
+      id: 'id-1',
+      mode: 'text',
+      title: 'Que profissão pareço ter?',
+      media: [
+        {
+          url: 'https://astrostyle.com/wp-content/uploads/2020/06/Capricorn-Woman-allef-vinicius-unsplash-scaled.jpg',
+        },
+      ],
+    },
+    {
+      id: 'id-2',
+      mode: 'binary',
+      title: 'Meu nariz é grande demais?',
+      media: [
+        {
+          url: 'https://bignoise.com/wp-content/uploads/2020/02/Stolar_artist_photo.png',
+        },
+      ],
+    },
+    {
+      id: 'id-3',
+      mode: 'comparison',
+      title: 'Qual praia mais linda?',
+      media: [
+        {
+          url: 'https://a.cdn-hotels.com/gdcs/production108/d1153/9bb4f4ff-afd9-4267-9e81-8fc370241afc.jpg',
+        },
+        {
+          url: 'https://s3q7j5f5.stackpathcdn.com/wp-content/uploads/2021/04/praia-belissima-de-maceio.jpg',
+        },
+      ],
+    },
+    {
+      id: 'id-4',
+      mode: 'multi',
+      title: 'Eu tenho cara de que?',
+      options: [
+        {text: 'Veterinária', number: 1},
+        {text: 'Modelo', number: 2},
+        {text: 'Revendedora Avon', number: 3},
+      ],
+      media: [
+        {
+          url: 'https://astrostyle.com/wp-content/uploads/2020/06/Capricorn-Woman-allef-vinicius-unsplash-scaled.jpg',
+        },
+      ],
+    },
+    {
+      id: 'id-5',
+      mode: 'video',
+      title: 'Qual o lance mais bonito',
+      options: [
+        {
+          text: '',
+          thumb:
+            'https://storage.googleapis.com/somnotalo/Screenshot%20from%202022-02-11%2023-31-33.png',
+          number: 1,
+        },
+        {
+          text: '',
+          thumb:
+            'https://storage.googleapis.com/somnotalo/Screenshot%20from%202022-02-11%2023-32-22.png',
+          number: 2,
+        },
+        {
+          text: '',
+          thumb:
+            'https://storage.googleapis.com/somnotalo/Screenshot%20from%202022-02-11%2023-32-57.png',
+          number: 3,
+        },
+        {
+          text: '',
+          thumb:
+            'https://storage.googleapis.com/somnotalo/Screenshot%20from%202022-02-11%2023-32-57.png',
+          number: 4,
+        },
+      ],
+      media: [
+        {
+          url: 'https://storage.googleapis.com/somnotalo/y2meta.com%20-%20One%20of%20the%20best%20goals%20in%20football%20%F0%9F%94%A5%F0%9F%90%90%20.mp4',
+        },
+        {
+          url: 'https://storage.googleapis.com/somnotalo/y2meta.com%20-%20When%20assist%20is%20more%20beautiful%20than%20goal%20.mp4',
+        },
+        {
+          url: 'https://storage.googleapis.com/somnotalo/y2meta.com-Best%20team%20goal%20%20_%20%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%20%23Shorts-(480p).mp4',
+        },
+      ],
+    },
+  ]);
   const [loading, setLoading] = useState(true);
 
   const getRandomQuestions = useCallback(async () => {
